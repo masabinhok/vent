3b8cc4108a763324c23df0b4ed6e619a6e6df6e5
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-07-26 00:15:40 +0545

    feat: define User and Session interfaces in types.ts for improved type safety

--- shared/types.ts
+++ shared/types.ts
@@ -1,3 +1,28 @@
 export type RequestStatus = 'pending' | 'active' | 'off'
 
 export type MessageType = 'success' | 'error' | 'warn' | 'info'
+
+
+export interface User {
+  id: number
+  fullName: string
+  email: string
+  role: 'USER' | 'ADMIN' | 'MODERATOR'
+  refreshToken?: string | null
+  createdAt: string // ISO date string
+  updatedAt: string
+}
+
+
+export interface Session {
+  id: number
+  user1Id: number
+  user2Id: number
+  user1: User
+  user2: User
+  startedAt: string  // ISO date string
+  expiresAt: string
+  status: 'ACTIVE' | 'ENDED'
+  friendshipStatus: 'PENDING' | 'ACCEPTED' | 'REJECTED'
+  updatedAt: string
+}
